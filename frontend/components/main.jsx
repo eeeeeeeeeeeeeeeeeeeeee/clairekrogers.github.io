362a217c1145a55de15a9548443501c035703b28
Claire Rogers <clairekrogers@gmail.com>
2017-04-02 17:45:39 -0700

    add react router

--- frontend/components/main.jsx
+++ frontend/components/main.jsx
@@ -1,23 +1,38 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 
 export default class Main extends React.Component {
   render() {
     return (
-      <section className="app-container">
-        <ul className="skill-list">
-          <li>About</li>
-          <li>Languages</li>
-          <li>Frameworks</li>
-          <li>Testing</li>
-          <li>Teaching</li>
-          <li>Sample Work</li>
-          <li>Contact</li>
-        </ul>
-        <footer>
-          <i className="fa fa-github" aria-hidden="true"></i>
-          <i className="fa fa-linkedin-square" aria-hidden="true"></i>
-        </footer>
-      </section>
+      <Router>
+        <section className="app-container">
+          <ul className="skill-list">
+            <li><Link to="/about">About</Link></li>
+            <li>Languages</li>
+            <li>Frameworks</li>
+            <li>Testing</li>
+            <li>Teaching</li>
+            <li>Sample Work</li>
+            <li>Contact</li>
+          </ul>
+          <footer>
+            <a href="https://github.com/clairekrogers">
+              <i className="fa fa-github" aria-hidden="true"></i>
+            </a>
+            <a href="https://linkedin.com">
+              <i className="fa fa-linkedin-square" aria-hidden="true"></i>
+            </a>
+          </footer>
+          <Route path="/about" component={About} />
+        </section>
+      </Router>
     );
   }
 };
+
+
+const About = () => (<div>hi</div>);
--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './components/main';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
   ReactDOM.render(<Main />, root);
+  /*
+  ReactDOM.render(
+    <Router>
+      <div>
+        <Route exact path="/" component={Main}/>
+      </div>
+    </Router>, root);*/
 });
+
+
+
+
